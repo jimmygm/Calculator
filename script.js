ce755f37c4d952032e872f80017277f0d35f82b0
Jimmy Garita <jimmy.garita.m@gmail.com>
2021-04-02 21:13:06 -0600

    Add display functionality

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+let numberButtons = document.querySelectorAll('.number-btn');
+let display = document.querySelector('#display span');
+let currentNum;
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', addNumber);
+});
+
+function addNumber(e) {
+    display.textContent += +this.textContent;
+    currentNum = +display.textContent;
+    console.log(currentNum);
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
 
-function substract(num1, num2) {
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
@@ -12,4 +26,8 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    return operator(num1, num2);
 }
