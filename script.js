b5e2b63229fba6fc1173c91ecd2e92084f6e5dc5
Jimmy Garita <jimmy.garita.m@gmail.com>
2021-04-02 22:52:23 -0600

    Add basic functionality

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
 let numberButtons = document.querySelectorAll('.number-btn');
-let display = document.querySelector('#display span');
-let currentNum;
+let operatorButtons = document.querySelectorAll('.operator-btn');
+let totalButton = document.querySelector('#total-btn');
+
+let numberDisplay = document.querySelector('#number-display');
+let numberCarry = document.querySelector('#number-carry');
+
+let currentNum = '';
+let lastNum;
+let operator;
 
 numberButtons.forEach((button) => {
     button.addEventListener('click', addNumber);
 });
 
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', addOperator);
+});
+
+totalButton.addEventListener('click', showResult);
+
 function addNumber(e) {
-    display.textContent += +this.textContent;
-    currentNum = +display.textContent;
-    console.log(currentNum);
+    currentNum += this.textContent;
+    showNumber(currentNum);
+}
+
+function addOperator(e) {
+    if (operator) {
+        lastNum = operate(lastNum, +currentNum, checkOperator(operator));
+        showNumber(lastNum);
+        currentNum = '';
+        operator = this.getAttribute('value');
+    }
+
+    if (!operator) {
+        operator = this.getAttribute('value');
+        lastNum = +currentNum;
+        currentNum = '';
+    }
+}
+
+function showResult(e) {
+    if (operator) {
+        lastNum = operate(lastNum, +currentNum, checkOperator(operator));
+        showNumber(lastNum);
+        currentNum = lastNum;
+        operator = false;
+    }
+}
+
+function checkOperator(string) {
+    switch (string) {
+        case 'add':
+            return add;
+        case 'subtract':
+            return subtract;
+        case 'multiply':
+            return multiply;
+        case 'divide':
+            return divide;
+    }
+}
+
+function showNumber(number = '') {
+    numberDisplay.textContent = number;
 }
 
 function add(num1, num2) {
